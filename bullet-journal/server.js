dd49fdbeb6a079900d3ed4a1b4c0cd1c1eba711e
Bijan Parvin <bparvin12@gmail.com>
2019-02-01 10:12:56 -0800

    created folders

---
+++ bullet-journal/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+//need to create a database in mongo called `bulletjournaldb`
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bulletjournaldb");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
