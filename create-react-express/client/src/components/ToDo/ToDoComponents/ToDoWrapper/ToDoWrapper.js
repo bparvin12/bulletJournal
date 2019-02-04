57464630ece6c789c99b742e0fb789004e567609
michelle <michellemchoe@gmail.com>
2019-02-04 14:14:32 -0800

    Working on header of 'to do' card. DID:   added header to 'to do' card.        filled in text for header. TO DO: add directions menu to the vertical dots

--- create-react-express/client/src/components/ToDo/ToDoComponents/ToDoWrapper/ToDoWrapper.js
+++ create-react-express/client/src/components/ToDo/ToDoComponents/ToDoWrapper/ToDoWrapper.js
@@ -1,20 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+// import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import "./ToDoWrapper.css"
-import ToDo from "../../Todo";
 
 const ToDoWrapper = props => {
-    const { classes } = props;
+    // const { classes } = props;
     
-    return <Card className={classes.card} >{props.children}</Card>;
+    return <Card className="toDoWrapper" >{props.children}</Card>;
 
 };
 
 
-ToDoWrapper.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(ToDoWrapper);
+export default ToDoWrapper;
