4605a47e4d290475380a8fc1149ab13823d6708b
michelle <michellemchoe@gmail.com>
2019-02-04 12:43:34 -0800

    creating an independent ToDo card with react. DID DID: created the files. connected them to each other. TO DO: format the sizing and look of the outer most div to get the general appearance of the card.
    :

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav"
+import ToDo from "./components/ToDo"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <ToDo />
       </div>
     );
   }
