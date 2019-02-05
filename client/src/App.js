9430e74de65c811e530a2ab36a1f04650006b36e
Bijan Parvin <bparvin12@gmail.com>
2019-02-05 12:12:02 -0800

    working

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,9 @@ import Home from "./pages/Home";
 import Finance from "./pages/Finance";
 import Goals from "./pages/Goals";
 import Health from "./pages/Health";
-// import Nav from "./components/Nav";
 import Weather from "./pages/Weather";
 import Menu from "./components/Menu";
 import "./App.css";
-// import PrimarySearchAppBar from "./components/Nav"
 
 
 const App = () => (
