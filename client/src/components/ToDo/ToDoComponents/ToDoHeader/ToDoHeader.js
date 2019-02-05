b2cffac53de15a091945ecbb9763a5111959b09d
michelle <michellemchoe@gmail.com>
2019-02-05 14:09:16 -0800

    To Do Widget HEADER DID:   added appropriate buttons. TO DO: stylize and add appropriate text.        send to separate functions

--- client/src/components/ToDo/ToDoComponents/ToDoHeader/ToDoHeader.js
+++ client/src/components/ToDo/ToDoComponents/ToDoHeader/ToDoHeader.js
@@ -1,45 +1,85 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Toolbar from '@material-ui/core/Toolbar';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
 import "./ToDoHeader.css";
 
 import Menu from '@material-ui/core/Menu';
 
-const ToDoHeader = () => (
-    <div className="toDoHeader">
+class ToDoHeader extends React.Component {
+    state = {
+        anchorEl: null,
+    };
 
-        <AppBar position="fixed" color="primary" className="toDoHeader-AppBar">
-            <Toolbar>
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
-                <Typography variant="h6" color="inherit" className="toDoHeader-Title">
-                    To Do List:
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+
+    AddTaskClick = () => {
+        console.log("add task clicked");
+    };
+
+
+    render() {
+        const { anchorEl } = this.state;
+        return (
+            <div className="toDoHeader" >
+
+                <div className="toDoHeader-AppBar">
+                    <Toolbar>
+
+                        <Typography variant="h6" color="inherit" className="toDoHeader-Title">
+                            To Do List:
                 </Typography>
-                <div>
-                    <Icon color="inherit">
-                        <i class="material-icons">add</i>
-                    </Icon>
-                    <Icon color="inherit">
-                        <i class="material-icons">more_vert</i>
-                    </Icon>
+                        <div>
+                            <IconButton onClick={this.AddTaskClick}>
+                                <i className="material-icons">add</i>
+                            </IconButton>
+
+                            <IconButton
+                                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                                aria-haspopup="true"
+                                onClick={this.handleClick}
+                            >
+                                <i className="material-icons">more_vert</i>
+                            </IconButton>
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={anchorEl}
+                                open={Boolean(anchorEl)}
+                                onClose={this.handleClose}
+                            >
+                                <MenuItem onClick={this.handleClose}>start</MenuItem>
+                                <MenuItem onClick={this.handleClose}>postpon</MenuItem>
+                                <MenuItem onClick={this.handleClose}>complete</MenuItem>
+                                <MenuItem onClick={this.handleClose}>cancel</MenuItem>
+                            </Menu>
+
+
+                        </div>
+                    </Toolbar>
                 </div>
-            </Toolbar>
-        </AppBar>
 
 
-    </div>
-);
+            </div>
+
+        );
+    }
+}
 
 
-ToDoHeader.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
+// ToDoHeader.propTypes = {
+//     classes: PropTypes.object.isRequired,
+// };
 
 export default ToDoHeader;
--- client/src/widgets/Todos/Todos.js
+++ client/src/widgets/Todos/Todos.js
@@ -2,17 +2,19 @@ import React from "react";
 import { withStyles } from '@material-ui/core/styles';
 import styles from './Todos.styles';
 import Button from '@material-ui/core/Button';
-
+import ToDo from '../../components/ToDo';
 
 const Todos = (props) => {
     const { classes } = props;
     return (
-        <div>
-            <h1 className={classes.input}>TO DO LIST!!</h1>
-            <Button variant="contained" className={classes.button}>
-                TODOS
-           </Button>
-        </div>
+        // <div>
+        //     <h1 className={classes.input}>TO DO LIST!!</h1>
+        //     <Button variant="contained" className={classes.button}>
+        //         TODOS
+        //    </Button>
+        // </div>
+
+        <ToDo />
     )
 }
 
