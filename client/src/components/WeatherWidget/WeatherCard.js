5b94d2b42969632379705cf6449b1cf7658e4757
Bijan Parvin <bparvin12@gmail.com>
2019-02-06 10:01:13 -0800

    merging

---
+++ client/src/components/WeatherWidget/WeatherCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+const styles = {
+    div: {
+        display: "flex"
+    },
+    iframe: {
+        display: "flex",
+    },
+    a: {
+        textIndent: "-9999em",
+        display: "flex",
+        margin: "-20px 0 0 0",
+        height: "20px",
+        zIndex: "1",
+        position: "relative"
+    }
+}
+
+const WeatherCard = () => (
+        <div style={styles.div}>
+            <iframe title="navbar weather" style={styles.iframe} src="https://cdnres.willyweather.com/widget/loadView.html?id=101901" height="230" frameBorder="0" scrolling="no"></iframe>
+            <a style={styles.a} href="https://www.willyweather.com/ca/orange-county/irvine.html" rel="nofollow">irvine Forecast</a>
+        </div>
+    
+
+);
+
+export default WeatherCard;
