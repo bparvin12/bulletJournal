2b281fd6aba2121e4c6f5d2d2a36a8e8eb0950d4
Cindy Chu <cindychu619@yahoo.com>
2019-02-11 10:01:09 -0800

    added LoginVisual page - still WIP

--- client/src/pages/LoginVisual/LoginVisual.js
+++ client/src/pages/LoginVisual/LoginVisual.js
@@ -9,10 +9,13 @@ import dog from "./Images/BG1.jpg"
 const LoginVisual = (props) => {
     const { classes } = props;
     return (
-        <div>
-            <h1>Q</h1>
+        <div className="container">
+            {/* <h1>Q</h1> */}
             {/* <h1 className="flip-horizontal-fwd">Q</h1> */}
+            <h1 className="bounce-in-top">Q</h1>
             {/* <img src={dog}className="logo flip-horizontal-fwd"></img> */}
+            {/* <h2>the Butler</h2> */}
+            <h2 className="tracking-in-expand">the Butler</h2>
             <input placeholder="e-mail" className="style-input"></input>
             <input placeholder="Password" className="style-input"></input>
             <p><button className="style-button">
