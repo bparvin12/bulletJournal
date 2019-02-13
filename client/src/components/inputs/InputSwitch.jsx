328bce549d9b256ad7c9f475481387b165d11824
Bijan Parvin <bparvin12@gmail.com>
2019-02-13 14:44:30 -0800

    commenting out unput folder

--- client/src/components/inputs/InputSwitch.jsx
+++ client/src/components/inputs/InputSwitch.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Switch from '@material-ui/core/Switch';
+// import React from 'react';
+// import Switch from '@material-ui/core/Switch';
 
-class InputSwitch extends React.Component {
-    state = {
-        checked: false,
-    };
+// class InputSwitch extends React.Component {
+//     state = {
+//         checked: false,
+//     };
 
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.checked });
-    };
+//     handleChange = name => event => {
+//         this.setState({ [name]: event.target.checked });
+//     };
 
-    render() {
-        return (
-            <Switch
-                checked={this.state.checked}
-                onChange={this.handleChange('checked')}
-                value="checkedA"
-                color={this.props.color}
-            />
-        );
-    }
-}
+//     render() {
+//         return (
+//             <Switch
+//                 checked={this.state.checked}
+//                 onChange={this.handleChange('checked')}
+//                 value="checkedA"
+//                 color={this.props.color}
+//             />
+//         );
+//     }
+// }
 
-export default InputSwitch;
+// export default InputSwitch;
