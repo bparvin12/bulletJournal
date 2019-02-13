a7eb36a691e9029a9b863686edcae7b15edd12a1
Bijan Parvin <bparvin12@gmail.com>
2019-02-13 12:40:01 -0800

    fixed drawer

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -12,15 +12,30 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import ListAltIcon from '@material-ui/icons/ListAlt'
+import EuroSymbolIcon from '@material-ui/icons/EuroSymbol'
+import IsoIcon from '@material-ui/icons/Iso'
+import WbSunnyIcon from '@material-ui/icons/WbSunny'
 import Navbar from "../Navbar";
 import styles from "./Menu.styles";
+import Switch from '@material-ui/core/Switch';
 
 
 class Menu extends React.Component {
   state = {
     open: false,
+    checkedA: true,
+    checkedB: true,
+    checkedC: true,
+    checkedD: true,
+    checkedE: true,
+    checkedF: true,
+    checkedG: true,
+    checkedH: true
   };
 
   handleDrawerOpen = () => {
@@ -31,6 +46,9 @@ class Menu extends React.Component {
     this.setState({ open: false });
   };
 
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
   render() {
     const { classes, theme } = this.props;
 
@@ -56,25 +74,90 @@ class Menu extends React.Component {
             <IconButton onClick={this.handleDrawerClose}>
               {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
             </IconButton>
-     
+
           </div>
           <Divider />
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            {/* performance */}
+            <ListItem button >
+              <ListItemIcon><TrendingUpIcon /></ListItemIcon>
+              <ListItemText>Performance</ListItemText>
+              <Switch
+                checked={this.state.checkedA}
+                onChange={this.handleChange('checkedA')}
+                value="checkedA"
+              />
+            </ListItem>
+            {/* task completion */}
+            <ListItem button >
+              <ListItemIcon><CheckBoxIcon /></ListItemIcon>
+              <ListItemText>Task Completion</ListItemText>
+              <Switch
+                checked={this.state.checkedB}
+                onChange={this.handleChange('checkedB')}
+                value="checkedB"
+              />
+            </ListItem>
+            {/* Health Stats */}
+            <ListItem button >
+              <ListItemIcon><AccessibilityNewIcon /></ListItemIcon>
+              <ListItemText>Health Stats</ListItemText>
+              <Switch
+                checked={this.state.checkedC}
+                onChange={this.handleChange('checkedC')}
+                value="checkedC"
+              />
+            </ListItem>
+            {/* Budget */}
+            <ListItem button >
+              <ListItemIcon><MonetizationOnIcon /></ListItemIcon>
+              <ListItemText>Budget</ListItemText>
+              <Switch
+                checked={this.state.checkedD}
+                onChange={this.handleChange('checkedD')}
+                value="checkedD"
+              />
+            </ListItem>
+            {/* Todo's */}
+            <ListItem button >
+              <ListItemIcon><ListAltIcon /></ListItemIcon>
+              <ListItemText>Todos</ListItemText>
+              <Switch
+                checked={this.state.checkedE}
+                onChange={this.handleChange('checkedE')}
+                value="checkedE"
+              />
+            </ListItem>
+            {/* CryptoCurrency */}
+            <ListItem button >
+              <ListItemIcon><EuroSymbolIcon /></ListItemIcon>
+              <ListItemText>CryptoCurrency</ListItemText>
+              <Switch
+                checked={this.state.checkedF}
+                onChange={this.handleChange('checkedF')}
+                value="checkedF"
+              />
+            </ListItem>
+             {/* Calculator */}
+             <ListItem button >
+              <ListItemIcon><IsoIcon /></ListItemIcon>
+              <ListItemText>Calculator</ListItemText>
+              <Switch
+                checked={this.state.checkedG}
+                onChange={this.handleChange('checkedG')}
+                value="checkedG"
+              />
+            </ListItem>
+            {/* Weather */}
+            <ListItem button >
+              <ListItemIcon><WbSunnyIcon /></ListItemIcon>
+              <ListItemText>Weather</ListItemText>
+              <Switch
+                checked={this.state.checkedH}
+                onChange={this.handleChange('checkedH')}
+                value="checkedH"
+              />
+            </ListItem>
           </List>
         </Drawer>
         <main className={classes.content}>
